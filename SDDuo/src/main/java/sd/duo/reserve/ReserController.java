47fd1a5bea87c7b8e5cca2f2c99da1aa4022e82b
YUN <rnzld90@gmail.com>
2017-07-04 14:51:35 +0900

    윤성오빠 예약 완료

--- SDDuo/src/main/java/sd/duo/reserve/ReserController.java
+++ SDDuo/src/main/java/sd/duo/reserve/ReserController.java
@@ -1,6 +1,10 @@
 package sd.duo.reserve;
 
+import java.util.List;
+
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -9,6 +13,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import sd.duo.common.Paging;
+
+
 
 @Controller
 @RequestMapping(value="/Reserve")
@@ -16,7 +23,98 @@ public class ReserController {
 	
 	@Resource
 	public ReserService reserService;
+	
+	private int currentPage = 1;
+	private int totalCount;
+	private int blockCount = 10;
+	private int blockPage = 5;
+	private String pagingHtml;
+	private Paging paging;
 	ModelAndView mv = new ModelAndView();
+	
+	   // 개인예약 리스트
+	   @RequestMapping(value="/OneReserList.do", method = RequestMethod.GET)
+	   public ModelAndView oneReserList(HttpServletRequest request){
+	      
+	      ModelAndView mv = new ModelAndView();
+	      
+	      List<OneReserModel> oneReserList = reserService.oneReserList();
+	      
+	      if(request.getParameter("currentPage") == null || request.getParameter("currentPage").trim().isEmpty()
+	            || request.getParameter("currentPage").equals("0")){
+	         currentPage = 1;
+	      }
+	      else{
+	         currentPage = Integer.parseInt(request.getParameter("currentPage"));
+	      }
+	      
+	      totalCount = oneReserList.size();
+	      
+	      paging = new Paging(currentPage, totalCount, blockCount, blockPage, "oneReserList");
+	      pagingHtml = paging.getPagingHtml().toString();
+	      
+	      int lastCount = totalCount;
+	      if(paging.getEndCount() < totalCount){
+	         lastCount = paging.getEndCount() + 1;
+	      }
+	      
+	      oneReserList = oneReserList.subList(paging.getStartCount(), lastCount);
+	      
+	      
+	      mv.addObject("list", oneReserList);
+	      mv.addObject("currentPage", currentPage);
+	      mv.addObject("pagingHtml", pagingHtml);
+	      mv.addObject("totalCount", totalCount);
+	      
+	      mv.setViewName("/reservation/oneReserList");
+	      
+	      return mv;
+	      
+	   }
+	
+	//그룹예약 리스트
+	@RequestMapping(value="/GrReserList.do",method = RequestMethod.GET)
+	public ModelAndView GrReserList(HttpServletRequest request){
+		ModelAndView mv = new ModelAndView();
+		
+		List<GroupReserModel> GrReserList = reserService.GrReserList();
+		
+		if (request.getParameter("currentPage") == null || request.getParameter("currentPage").trim().isEmpty()
+				|| request.getParameter("currentPage").equals("0")) {
+			currentPage = 1;
+		}
+		else {
+			currentPage = Integer.parseInt(request.getParameter("currentPage"));
+		}
+		
+		totalCount = GrReserList.size();
+		
+		paging = new Paging(currentPage, totalCount, blockCount, blockPage, "movieList");
+		pagingHtml = paging.getPagingHtml().toString();
+		
+		int lastCount = totalCount;
+		//System.out.println(paging.getEndCount());
+		//System.out.println(totalCount);
+		if (paging.getEndCount() < totalCount) {
+			lastCount = paging.getEndCount() + 1;
+		}
+
+		GrReserList = GrReserList.subList(paging.getStartCount(), lastCount);
+		
+
+		
+		
+		mv.addObject("list", GrReserList);
+		mv.addObject("currentPage", currentPage);
+		mv.addObject("pagingHtml", pagingHtml);
+		mv.addObject("totalCount", totalCount);
+		
+		System.out.println(GrReserList.size());
+		
+		
+		mv.setViewName("/reservation/grReserList");
+		return mv;
+	}
 	
 	// 개인예약 폼
 	@RequestMapping(value="/OneReserveForm.do", method = RequestMethod.GET)
@@ -35,7 +133,7 @@ public class ReserController {
 			return mv;
 		}*/
 		reserService.insertOneReser(oneReserModel);
-		mv.setViewName("/reservation/oneReserList");
+		mv.setViewName("redirect:/Reserve/OneReserList.do");
 		return mv;
 	}
 	
@@ -57,7 +155,7 @@ public class ReserController {
 				return mv;
 			}*/
 			reserService.insertGroupReser(groupReserModel);
-			mv.setViewName("/reservation/oneReserList");
+			mv.setViewName("redirect:/Reserve/GrReserList.do");
 			return mv;
 		}
 }
--- SDDuo/src/main/java/sd/duo/reserve/ReserDAO.java
+++ SDDuo/src/main/java/sd/duo/reserve/ReserDAO.java
@@ -1,5 +1,7 @@
 package sd.duo.reserve;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 @Repository(value="oneReserDAO")
@@ -14,4 +16,10 @@ public interface ReserDAO {
 	
 	/*그룹예약 등록*/
 	public int insertGroupReser(GroupReserModel groupReserModel);
+	
+	// 개인예약 목록
+	List<OneReserModel> oneReserList();
+	
+	// 그룹예약 목록
+	List<GroupReserModel> GrReserList();
 }
