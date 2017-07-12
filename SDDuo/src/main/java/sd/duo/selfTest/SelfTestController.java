485966a6e8267cea30b09e77363bfdf2a78ec787
ehdgks01 <donghan0109904@gmail.com>
2017-07-12 15:13:04 +0900

    자가진단 동한

--- SDDuo/src/main/java/sd/duo/selfTest/SelfTestController.java
+++ SDDuo/src/main/java/sd/duo/selfTest/SelfTestController.java
@@ -18,6 +18,8 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class SelfTestController {
 	
+	private String category;
+	
 	
 	@Resource
 	private SelfTestService selfService;
@@ -26,7 +28,6 @@ public class SelfTestController {
 	
 	@RequestMapping(value="/self/selfWrite.do", method=RequestMethod.GET)
 	public ModelAndView noticeForm(HttpServletRequest request) {
-		System.out.println("111");
 		
 		ModelAndView mav = new ModelAndView();
 		mav.addObject("selfModel", new SelfTestModel());
@@ -36,57 +37,229 @@ public class SelfTestController {
 	
 	// 자가진단 점수 입력
 	@RequestMapping(value="/self/selfWrite.do", method=RequestMethod.POST)
-	public ModelAndView noticeWrite(@ModelAttribute("selfModel") SelfTestModel selfModel, HttpServletRequest request)throws Exception{
-		System.out.println("222");
+	public ModelAndView noticeWrite1(SelfTestModel selfModel, HttpServletRequest requeset,
+			HttpSession session)throws Exception{
+		System.out.println("111");
+		
 		ModelAndView mav = new ModelAndView();
 		
-		String m_id="1";
+		String id;
+		String cg;
 		
-		System.out.println(m_id);
 		
-		if(m_id != null){
+		if(	session.getAttribute("session_member_id") != null){
 			
-			System.out.println("333아이디가 있다");
-			String t_category = selfModel.getT_category();
+			System.out.println("222");
+			id = session.getAttribute("session_member_id").toString();
 			
-			if(t_category != null){
-				System.out.println("333아이디가 있고 카테고리도 있다");
-				if(t_category == "1"){
-					System.out.println("4444카테고리가 1");
-					selfService.selfModify1(m_id);
-					
-				}else if(t_category == "2"){
-					System.out.println("4444카테고리가 2");
-					selfService.selfModify2(m_id);
-					
-				}else if(t_category == "3"){
-					System.out.println("4444카테고리가 3");
-					selfService.selfModify3(m_id);
-					
-				}else if(t_category == "4"){
-					System.out.println("4444카테고리가 4");
-					selfService.selfModify4(m_id);
-					
-				}
+		}
+
+		
+		id = requeset.getParameter("m_id").toString(); // 세션아이디로 안하면 널 포인트 오류가 뜨기때문에 나중에 세션아이디로 바꿀것
+		
+		cg = requeset.getParameter("category").toString();
+		
+		System.out.println("222"+cg+id);
+		System.out.println("3333"+selfModel.getT_score());
+		
+/*		String sc = requeset.getParameter("t_score");
+		
+		int score = Integer.parseInt(sc);*/
+
+		
+		if(cg.equals("1")){
+			
+			
+			
+			System.out.println("111111"+selfModel.getCount());
+			System.out.println("3333333"+selfModel.getT_score());
+			
+			int count = selfService.check1(id);
+			
+		
+			
+			System.out.println("카운트"+count);
+			
+			if(count != 0){
+			selfService.selfModify1(selfModel);
+			
+			mav.setViewName("self");
+		
+			return mav;
+			
+			}else{
+				
+				
+				selfService.selfWrite1(selfModel);
+				
+				mav.setViewName("self");
+			
+				return mav;
 				
 			}
+		}else if(cg.equals("2")){
+			
+		
+			if(selfModel.getCount() != 0){
+				
+				selfService.selfModify2(id);
+				
+				mav.setViewName("self");
+			
+				return mav;
+				
+				}else{
+					
+					
+					selfService.selfWrite2(selfModel);
+					
+					mav.setViewName("self");
+				
+					return mav;
+					
+				}
+			
+			
+		}else if(cg.equals("3")){
+			
+		
+			selfService.newtechnology3(selfModel);
+			System.out.println("333"+selfModel);
+			
+			mav.setViewName("self");
+		
+			return mav;
+		}else{
+			
+		
+			selfService.newtechnology4(selfModel);
+			System.out.println("444"+selfModel);
+			
+			mav.setViewName("self");
+		
+			return mav;
+		}
+		
+		
+		
+	}
+	
+	
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	
+	/*// 자가진단 점수 입력
+		@RequestMapping(value="/self/selfWrite2.do", method=RequestMethod.POST)
+		public ModelAndView noticeWrite2(@ModelAttribute("selfModel") SelfTestModel selfModel, HttpServletRequest requeset)throws Exception{
+			System.out.println("222");
+			
+			ModelAndView mav = new ModelAndView();
+			
+			String m_id;
+			
+			m_id = requeset.getParameter("m_id");
+			
+			selfModel = (SelfTestModel)selfService.check2(m_id);
+			
+			System.out.println("222"+selfModel.getM_id());
+			
+			
+			if(selfModel.getM_id() != null){
+			
+			selfService.selfModify2(m_id);
 			
 			mav.setViewName("self");
 			
 			return mav;
 			
-		}else{
+			}else{
 			
-		System.out.println("5555처음 입력");
-		
-		selfService.selfWrite(selfModel);
+			selfService.selfWrite2(selfModel);
 			
-		mav.setViewName("self");
-				
-		return mav;
-		
+			mav.setViewName("self");
+					
+			return mav;
+			}
+			
+			
 		}
 		
-	}
+		// 자가진단 점수 입력
+		@RequestMapping(value="/self/selfWrite3.do", method=RequestMethod.POST)
+		public ModelAndView noticeWrite3(@ModelAttribute("selfModel") SelfTestModel selfModel, HttpServletRequest requeset)throws Exception{
+			System.out.println("222");
+			
+			ModelAndView mav = new ModelAndView();
+			
+			String m_id;
+			
+			m_id = requeset.getParameter("m_id");
+			
+			selfModel = (SelfTestModel)selfService.check3(m_id);
+			
+			System.out.println("222"+selfModel.getM_id());
+			
+			
+			if(selfModel.getM_id() != null){
+			
+			selfService.selfModify3(m_id);
+			
+			mav.setViewName("self");
+			
+			return mav;
+			
+			}else{
+			
+			selfService.selfWrite3(selfModel);
+			
+			mav.setViewName("self");
+					
+			return mav;
+			}
+			
+			
+		}
+		
+		// 자가진단 점수 입력
+		@RequestMapping(value="/self/selfWrite4.do", method=RequestMethod.POST)
+		public ModelAndView noticeWrite4(@ModelAttribute("selfModel") SelfTestModel selfModel, HttpServletRequest requeset)throws Exception{
+			System.out.println("222");
+			
+			ModelAndView mav = new ModelAndView();
+			
+			String m_id;
+			
+			m_id = requeset.getParameter("m_id");
+			
+			selfModel = (SelfTestModel)selfService.check4(m_id);
+			
+			System.out.println("222"+selfModel.getM_id());
+			
+			
+			if(selfModel.getM_id() != null){
+			
+			selfService.selfModify4(m_id);
+			
+			mav.setViewName("self");
+			
+			return mav;
+			
+			}else{
+			
+			selfService.selfWrite4(selfModel);
+			
+			mav.setViewName("self");
+					
+			return mav;
+			}
+			
+			
+		}*/
 
 }
--- SDDuo/src/main/java/sd/duo/selfTest/SelfTestService.java
+++ SDDuo/src/main/java/sd/duo/selfTest/SelfTestService.java
@@ -16,45 +16,136 @@ public class SelfTestService implements SelfTestDAO{
 	private SqlSessionTemplate sqlSessionTemplate;
 	
 		@Override
-		public List<SelfTestModel> selfList() {
+		public List<SelfTestModel> selfList1(String m_id) {
 		
-			return sqlSessionTemplate.selectList("self.selfList");
+			return sqlSessionTemplate.selectList("self.selfList1", m_id);
 		}
 	
 		//글쓰기
 		@Override
-		public int selfWrite(SelfTestModel selfModel) {
-			return sqlSessionTemplate.insert("self.selfWrite", selfModel);
+		public int selfWrite1(SelfTestModel selfModel) {
+			return sqlSessionTemplate.insert("self.selfWrite1", selfModel);
 		}
 		
 		//글 하나 불러오기
 		@Override
-		public SelfTestModel check(String m_id) {
-			return sqlSessionTemplate.selectOne("self.check", m_id); 
+		public int check1(String m_id) {
+			return sqlSessionTemplate.selectOne("self.check1", m_id); 
 		}
 		
 		//카테고리1 업데이트
 		@Override
-		public Object selfModify1(String m_id) {
-			return sqlSessionTemplate.update("self.selfModify1", m_id); 
+		public Object selfModify1(SelfTestModel selfModel) {
+			return sqlSessionTemplate.update("self.selfModify1", selfModel); 
 		}
 		
-		//카테고리2 업데이트
+
+		@Override
+		public List<SelfTestModel> selfList2(String m_id) {
+		
+			return sqlSessionTemplate.selectList("self.selfList2", m_id);
+		}
+	
+		//글쓰기
+		@Override
+		public int selfWrite2(SelfTestModel selfModel) {
+			return sqlSessionTemplate.insert("self.selfWrite2", selfModel);
+		}
+		
+		//글 하나 불러오기
+		@Override
+		public SelfTestModel check2(String m_id) {
+			return sqlSessionTemplate.selectOne("self.check2", m_id); 
+		}
+		
+		//카테고리1 업데이트
 		@Override
 		public Object selfModify2(String m_id) {
 			return sqlSessionTemplate.update("self.selfModify2", m_id); 
 		}
 		
-		//카테고리3 업데이트
+		
+		//3
+		
+		@Override
+		public List<SelfTestModel> selfList3(String m_id) {
+		
+			return sqlSessionTemplate.selectList("self.selfList3", m_id);
+		}
+	
+		//글쓰기
+		@Override
+		public int selfWrite3(SelfTestModel selfModel) {
+			return sqlSessionTemplate.insert("self.selfWrite3", selfModel);
+		}
+		
+		//글 하나 불러오기
+		@Override
+		public SelfTestModel check3(String m_id) {
+			return sqlSessionTemplate.selectOne("self.check3", m_id); 
+		}
+		
+		//카테고리1 업데이트
 		@Override
 		public Object selfModify3(String m_id) {
 			return sqlSessionTemplate.update("self.selfModify3", m_id); 
 		}
 		
-		//카테고리4 업데이트
+		
+		//4
+		
+		@Override
+		public List<SelfTestModel> selfList4(String m_id) {
+		
+			return sqlSessionTemplate.selectList("self.selfList4", m_id);
+		}
+	
+		//글쓰기
+		@Override
+		public int selfWrite4(SelfTestModel selfModel) {
+			return sqlSessionTemplate.insert("self.selfWrite4", selfModel);
+		}
+		
+		//글 하나 불러오기
+		@Override
+		public SelfTestModel check4(String m_id) {
+			return sqlSessionTemplate.selectOne("self.check4", m_id); 
+		}
+		
+		//카테고리1 업데이트
 		@Override
 		public Object selfModify4(String m_id) {
 			return sqlSessionTemplate.update("self.selfModify4", m_id); 
 		}
+		
+		
+		
+		@Override
+		public int newtechnology1(SelfTestModel selfModel) {
+			
+			int a =sqlSessionTemplate.insert("self.newtechnology1", selfModel);
+			System.out.println(a);
+			return sqlSessionTemplate.insert("self.newtechnology1", selfModel);
+		}
+		
+		@Override
+		public int newtechnology2(SelfTestModel selfModel) {
+			return sqlSessionTemplate.insert("self.newtechnology2", selfModel);
+		}
+		
+		@Override
+		public int newtechnology3(SelfTestModel selfModel) {
+			return sqlSessionTemplate.insert("self.newtechnology3", selfModel);
+		}
+		
+		@Override
+		public int newtechnology4(SelfTestModel selfModel) {
+			return sqlSessionTemplate.insert("self.newtechnology4", selfModel);
+		}
+		
+		
+		
+		
+		
 	
 }
